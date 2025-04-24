5a249c7bc1c6056e686ed83f5881946981c5503e
AppaiahKelowna <appaiahtwokelowna@gmail.com>
2025-04-24 15:35:50 -0700

    adding Appaiah Profile source code

---
+++ Pro.js
@@ -0,0 +1,6 @@
+document.getElementById("btn_addlink").addEventListener("click",function() {
+    const linkContainer = document.getElementById("profile-container-right").querySelector("div");;
+    const newLink = document.createElement("a");
+    newLink.innerHTML = '<a><i class="fas fa-file-alt"></i>New link</a>';
+    linkContainer.insertBefore(newLink, document.getElementById("btn_addlink"));
+});
