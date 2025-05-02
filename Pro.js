7a3255a3eb53a9bc7aae4c5428e640b3b8854584
AppaiahKelowna <appaiahtwokelowna@gmail.com>
2025-05-02 13:21:57 -0700

    1. remove UI added. 2. template Ui added 3. add template element logic 4. remove template logic

--- Pro.js
+++ Pro.js
@@ -1,6 +1,18 @@
 document.getElementById("btn_add_link").addEventListener("click",function() {
-    const linkContainer = document.getElementById("profile-container-right").querySelector("div");;
-    const newLink = document.createElement("a");
-    newLink.innerHTML = '<a><i class="fas fa-file-alt"></i>New link</a>';
-    linkContainer.insertBefore(newLink, document.getElementById("btn_add_link"));
+    const containerRight = document.getElementById("profile-container-right");
+    const template = document.getElementById("template-link-wrapper");
+    const clone = template.content.cloneNode(true);
+    
+    
+    const removeLink = clone.querySelector(".remove-link"); 
+    removeLink.addEventListener("click", function() {
+        const linkWrapper = clone.querySelector(".link-wrapper");  
+        if(linkWrapper) {
+            containerRight.removeChild(linkWrapper);
+        }
+    })
+
+    containerRight.appendChild(clone); // append the child.
+
 });
+
