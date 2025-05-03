c0276312be868c954a91a1e8ea7d2cbad79933f8
AppaiahKelowna <appaiahtwokelowna@gmail.com>
2025-05-02 17:19:35 -0700

    getting remove-link from the actual DOM

--- Pro.js
+++ Pro.js
@@ -2,17 +2,12 @@ document.getElementById("btn_add_link").addEventListener("click",function() {
     const containerRight = document.getElementById("profile-container-right");
     const template = document.getElementById("template-link-wrapper");
     const clone = template.content.cloneNode(true);
-    
-    
-    const removeLink = clone.querySelector(".remove-link"); 
-    removeLink.addEventListener("click", function() {
-        const linkWrapper = clone.querySelector(".link-wrapper");  
-        if(linkWrapper) {
-            containerRight.removeChild(linkWrapper);
-        }
-    })
-
     containerRight.appendChild(clone); // append the child.
 
+});
+
+document.getElementById("remove-link").addEventListener("click",function(){
+    console.log("Remove link clicked");
+    
 });
 
